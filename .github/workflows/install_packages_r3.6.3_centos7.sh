7b67e12be7d33c2b9ccc68230630ed7c0992d3e2
Yuri05 <Yuri05@github.com>
2021-06-04 18:16:33 +0200

    osp 3.6.3

--- .github/workflows/install_packages_r3.6.3_centos7.sh
+++ .github/workflows/install_packages_r3.6.3_centos7.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Get software
-wget -nv  https://github.com/Open-Systems-Pharmacology/rClr/releases/download/v0.8.3_Linux/rClr_0.8.3_CentOS7.tar.gz /tmp_setup/
-wget -nv  https://github.com/Open-Systems-Pharmacology/OSPSuite-R/releases/download/v10.0.25/ospsuite_10.0.25_centOS7.tar.gz /tmp_setup/
-wget -nv  https://github.com/Open-Systems-Pharmacology/TLF-Library/releases/download/v1.2.4/tlf_1.2.4.tar.gz /tmp_setup/
-wget -nv  https://github.com/Open-Systems-Pharmacology/OSPSuite.ReportingEngine/releases/download/v1.2.40/ospsuite.reportingengine_1.2.40.tar.gz /tmp_setup/
+wget -nv https://github.com/Open-Systems-Pharmacology/rClr/releases/download/v0.8.3_Linux/rClr_0.8.3_CentOS7.tar.gz -P /tmp_setup/
+wget -nv https://github.com/Open-Systems-Pharmacology/OSPSuite-R/releases/download/v10.0.25/ospsuite_10.0.25_centOS7.tar.gz -P /tmp_setup/
+wget -nv https://github.com/Open-Systems-Pharmacology/TLF-Library/releases/download/v1.2.4/tlf_1.2.4.tar.gz -P /tmp_setup/
+wget -nv https://github.com/Open-Systems-Pharmacology/OSPSuite.ReportingEngine/releases/download/v1.2.40/ospsuite.reportingengine_1.2.40.tar.gz -P /tmp_setup/
 ls -la /tmp_setup/
 
 # Install packages
